d12f22511caf2af09de42accb4da3dd47420b5bd
ByloTonix <64357478+ByloTonix@users.noreply.github.com>
2023-12-08 18:34:59 +0300

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -5,28 +5,39 @@ import io
 
 app = Flask(__name__)
 
-data = pd.read_csv("../data/crime.csv", parse_dates=["month"], dayfirst=True)
-crime_sum = str(data['Total_crimes'].sum())
-data_info = str(data.mean(numeric_only=True))
+df = pd.read_csv("../data/crime.csv", parse_dates=["month"], dayfirst=True)
+crime_sum = int(df['Total_crimes'].sum())
+data_info = str(df.mean(numeric_only=True)).split()
+x = [[data_info[i], data_info[i+1]] for i in range(0, len(data_info), 2)][:-1]
+s = ""
+for i, j in x:
+    s += f"{i}: {j} \n"
+date = df["month"].dt.year
 
 @app.route('/')
 def index():
-    return "Flask Server is running!"
+    return "Finally! This piece of code (Flask Server) is running!"
 
 @app.route('/process_data', methods=['POST'])
 def process_data():
     data = request.get_json()
     action = data.get('action')
+
     if data.get('year'):
         year = data.get('year')
+
     if action == 'get_crimes':
-        processed_data = crime_sum
+        fig = px.line(df, x='month', y='Total_crimes', title='Total Crimes Over Time')
+        img_bytes = io.BytesIO()
+        fig.write_image(img_bytes, format='png')
+        img_bytes.seek(0)
+        return send_file(img_bytes, mimetype='image/png')
+
     elif action == 'get_dataset_info':
-        processed_data = data_info
+        processed_data = s
     elif action == 'get_graph':
-        data = pd.read_csv("../data/crime.csv", parse_dates=["month"], dayfirst=True)
-        date = data["month"].dt.year
-        filtered_data = data[date == year]
+        
+        filtered_data = df[date == year]
         crime_data = filtered_data.iloc[:, 2:].sum().sort_values(ascending=False)
         fig = px.bar(
             x=crime_data.index,
@@ -34,7 +45,7 @@ def process_data():
             labels={"x": "Crime Types", "y": "Total Crimes"},
             title=f"Crimes in {year}",
             color=crime_data.index,
-            height=500,
+            height=500
         )
         img_bytes = io.BytesIO()
         fig.write_image(img_bytes, format='png')
